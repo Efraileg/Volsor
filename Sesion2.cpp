62cfbbf6b0b2ff95724868d66a7dcb6f9ea9d0b5
Platinum <94174098+Efraileg@users.noreply.github.com>
2022-06-20 22:39:19 +0200

    Add files via upload

---
+++ Sesion2.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// ----------- PRACTICAS DE FUNDAMENTOS DE REDES DE COMUNICACIONES -----------
+// ---------------------------- CURSO 2021/22 --------------------------------
+// ----------------------------- SESION2.CPP ---------------------------------
+//============================================================================
+
+#include <stdio_ext.h>
+#include "selector.h"
+
+//using namespace std;
+
+int main()
+{
+
+  //Creación de la clase selectora.
+  selector S;
+  
+
+  //metodo inicial.
+  //Permite observar las interfaces disponibles y seleccionar una de ellas.
+  S.mostrarInterfaces();
+
+  //Una vez seleccionada una interfaz, escoger el grupo y tipo de funcionamiento que seguirá.
+  S.establecerComunicacion();
+
+  return 0;
+}
